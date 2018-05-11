3d7be16b518198cf902ced4d8fe0a3a89c4608da
Wai Tak Liu <waitakliu@yahoo.co.uk>
2018-05-11 23:27:29 +0100

    refactor and print error if cmd fails

--- testfile.py
+++ testfile.py
@@ -1,18 +1,31 @@
 #! /usr/bin/python
 
+from __future__ import print_function
 import os, sys
-import shlex, subprocess
+import shlex
+import subprocess
 import argparse
 
-try:
-    arg1 = sys.argv[1]
-except:
-    sys.exit(1)
+def main():
+    """
+    execute arguments provided.
+    needs better checking before execution!
+    """
+    try:
+        arg1 = sys.argv[1]
+    except:
+        sys.exit(1)
+    
+    argstr = " ".join(sys.argv[1:])
+    cmd = shlex.split(argstr)
+    
+    try:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        if out:
+            print(out)
+    except OSError as e:
+        print("ERROR: command '{}' -> {}".format(argstr, e))
 
-cmd = shlex.split(" ".join(sys.argv[1:]))
-
-proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-out, err = proc.communicate()
-if out:
-    print(out)
-
+if __name__ == "__main__":
+    main()
